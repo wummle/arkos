c91f7b7015dd4566036d59317331ebd55c5fe333
Wayne Lymbery <wayne.lymbery@envirosoft.co>
2021-11-17 14:05:24 +1100

    Update file

--- Update-RG351P.sh
+++ Update-RG351P.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 clear
 
-UPDATE_DATE="10262021-1"
+UPDATE_DATE="11172021-1"
 LOG_FILE="/home/ark/update$UPDATE_DATE.log"
 UPDATE_DONE="/home/ark/.config/.update$UPDATE_DATE"
 
@@ -166,6 +166,39 @@ if [ ! -f "/home/ark/.config/.update10262021" ]; then
 	sudo sed -i "/title\=/c\title\=ArkOS 351P/M wummle fork" /usr/share/plymouth/themes/text.plymouth
 
 	touch "/home/ark/.config/.update10262021"
+fi
+
+
+if [ ! -f "/home/ark/.config/.update11172021" ]; then
+
+	printf "\nRetroArch 1.9.13\n" | tee -a "$LOG_FILE"
+	sudo wget --no-check-certificate https://github.com/wummle/arkos/raw/main/11172021/arkosupdate11172021.zip -O /home/ark/arkosupdate11172021.zip -a "$LOG_FILE" || rm -f /home/ark/arkosupdate11172021.zip | tee -a "$LOG_FILE"
+	if [ -f "/home/ark/arkosupdate11172021.zip" ]; then
+		sudo unzip -X -o /home/ark/arkosupdate11172021.zip -d / | tee -a "$LOG_FILE"
+		sudo cp -f -v /usr/bin/emulationstation/emulationstation.351v /usr/bin/emulationstation/emulationstation | tee -a "$LOG_FILE"
+		sudo chmod 777 /usr/bin/emulationstation/emulationstation
+		sudo rm -v /usr/bin/emulationstation/emulationstation.351v | tee -a "$LOG_FILE"
+		sudo rm -v /home/ark/arkosupdate11172021.zip | tee -a "$LOG_FILE"
+	else 
+		printf "\nThe update couldn't complete because the package did not download correctly.\nPlease retry the update again." | tee -a "$LOG_FILE"
+		sleep 3
+		echo $c_brightness > /sys/devices/platform/backlight/backlight/backlight/brightness
+		exit 1
+	fi
+
+	printf "\nRemove old logs, cache and backup folder files from var folder\n" | tee -a "$LOG_FILE"
+	sudo rm -rfv /var/cache/* | tee -a "$LOG_FILE"
+	sudo rm -rfv /var/backups/* | tee -a "$LOG_FILE"
+	sudo journalctl --vacuum-time=1s | tee -a "$LOG_FILE"
+
+	printf "\nEnsure 64bit and 32bit sdl2 is still properly linked\n" | tee -a "$LOG_FILE"
+	sudo ln -sfv /usr/lib/aarch64-linux-gnu/libSDL2-2.0.so.0.14.1 /usr/lib/aarch64-linux-gnu/libSDL2-2.0.so.0 | tee -a "$LOG_FILE"
+	sudo ln -sfv /usr/lib/arm-linux-gnueabihf/libSDL2-2.0.so.0.10.0 /usr/lib/arm-linux-gnueabihf/libSDL2-2.0.so.0 | tee -a "$LOG_FILE"
+
+	printf "\nUpdate boot text to reflect final current version of ArkOS for the 351 P/M \n" | tee -a "$LOG_FILE"
+	sudo sed -i "/title\=/c\title\=ArkOS 351P/M wummle fork" /usr/share/plymouth/themes/text.plymouth
+
+	touch "/home/ark/.config/.update11172021"
 fi
 
 if [ ! -f "$UPDATE_DONE" ]; then
