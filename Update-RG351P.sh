2670d7e66b17806b0d4a36e4cc6a68dc4b62eee5
Slayer366 <andykratz@hotmail.com>
2024-11-06 17:55:23 -0600

    Update

--- Update-RG351P.sh
+++ Update-RG351P.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 clear
 
-UPDATE_DATE="11052024"
+UPDATE_DATE="11062024"
 LOG_FILE="/home/ark/update$UPDATE_DATE.log"
 UPDATE_DONE="/home/ark/.config/.update$UPDATE_DATE"
 
@@ -1735,10 +1735,6 @@ if [ ! -f "/home/ark/.config/.update07052024" ]; then
       sudo ln -sfv /usr/lib/arm-linux-gnueabihf/libSDL2-2.0.so.0 /usr/lib/arm-linux-gnueabihf/libSDL2-2.0.so | tee -a "$LOG_FILE"
       sudo ln -sfv /usr/lib/arm-linux-gnueabihf/libSDL2-2.0.so.0.3000.3 /usr/lib/arm-linux-gnueabihf/libSDL2.so | tee -a "$LOG_FILE"
 
-
-	printf "\nUpdate boot text to reflect final current version of ArkOS for the 351 P/M \n" | tee -a "$LOG_FILE"
-	#sudo sed -i "/title\=/c\title\=ArkOS 351P/M wuMMLe gaming & Slayer366" /usr/share/plymouth/themes/text.plymouth
-
 	touch "/home/ark/.config/.update07052024"
 
 fi
@@ -1747,16 +1743,6 @@ fi
 if [ ! -f "/home/ark/.config/.update09082024" ]; then
 
 	printf "\nInstall and Update Python and Python utilities (already present in July 5th image), Install dos2unix \n" | tee -a "$LOG_FILE"
-	#sudo wget --no-check-certificate https://github.com/wummle/arkos/raw/main/09082024/arkosupdate09082024.zip -O /home/ark/arkosupdate09082024.zip -a "$LOG_FILE" || rm -f /home/ark/arkosupdate09082024.zip | tee -a "$LOG_FILE"
-	#if [ -f "/home/ark/arkosupdate09082024.zip" ]; then
-	#	sudo unzip -X -o /home/ark/arkosupdate09082024.zip -d / | tee -a "$LOG_FILE"
-	#	sudo rm -v /home/ark/arkosupdate09082024.zip | tee -a "$LOG_FILE"
-	#else
-	#	printf "\nThe update couldn't complete because the package did not download correctly.\nPlease retry the update again." | tee -a "$LOG_FILE"
-	#	sleep 3
-	#	echo $c_brightness > /sys/devices/platform/backlight/backlight/backlight/brightness
-	#	exit 1
-	#fi
 
       sudo chown -R ark:ark /opt/
 
@@ -1785,10 +1771,6 @@ if [ ! -f "/home/ark/.config/.update09082024" ]; then
       sudo ln -sfv /usr/lib/arm-linux-gnueabihf/libSDL2.so /usr/lib/arm-linux-gnueabihf/libSDL2-2.0.so.0 | tee -a "$LOG_FILE"
       sudo ln -sfv /usr/lib/arm-linux-gnueabihf/libSDL2-2.0.so.0 /usr/lib/arm-linux-gnueabihf/libSDL2-2.0.so | tee -a "$LOG_FILE"
       sudo ln -sfv /usr/lib/arm-linux-gnueabihf/libSDL2-2.0.so.0.3000.3 /usr/lib/arm-linux-gnueabihf/libSDL2.so | tee -a "$LOG_FILE"
-
-
-	printf "\nUpdate boot text to reflect final current version of ArkOS for the 351 P/M \n" | tee -a "$LOG_FILE"
-	#sudo sed -i "/title\=/c\title\=ArkOS 351P/M wuMMLe gaming & Slayer366" /usr/share/plymouth/themes/text.plymouth
 
 	touch "/home/ark/.config/.update09082024"
 
@@ -1837,11 +1819,29 @@ if [ ! -f "/home/ark/.config/.update11052024" ]; then
       sudo ln -sfv /usr/lib/arm-linux-gnueabihf/libSDL2-2.0.so.0 /usr/lib/arm-linux-gnueabihf/libSDL2-2.0.so | tee -a "$LOG_FILE"
       sudo ln -sfv /usr/lib/arm-linux-gnueabihf/libSDL2-2.0.so.0.3000.7 /usr/lib/arm-linux-gnueabihf/libSDL2.so | tee -a "$LOG_FILE"
 
-
-	printf "\nUpdate boot text to reflect final current version of ArkOS for the 351 P/M \n" | tee -a "$LOG_FILE"
-	#sudo sed -i "/title\=/c\title\=ArkOS 351P/M wuMMLe gaming & Slayer366" /usr/share/plymouth/themes/text.plymouth
-
 	touch "/home/ark/.config/.update11052024"
+
+fi
+
+
+if [ ! -f "/home/ark/.config/.update11062024" ]; then
+
+	printf "\nSet RetroArch screenshots to save in content directory \n" | tee -a "$LOG_FILE"
+
+      sed -i '/screenshots_in_content_dir \= \"false\"/c\screenshots_in_content_dir \= \"true\"' /home/ark/.config/retroarch/retroarch.cfg
+      sed -i '/screenshots_in_content_dir \= \"false\"/c\screenshots_in_content_dir \= \"true\"' /home/ark/.config/retroarch/retroarch.cfg.bak
+      sed -i '/screenshots_in_content_dir \= \"false\"/c\screenshots_in_content_dir \= \"true\"' /home/ark/.config/retroarch32/retroarch.cfg
+      sed -i '/screenshots_in_content_dir \= \"false\"/c\screenshots_in_content_dir \= \"true\"' /home/ark/.config/retroarch32/retroarch.cfg.bak
+
+    printf "\nEnsure 64bit and 32bit SDL2 are still properly linked\n" | tee -a "$LOG_FILE"
+      sudo ln -sfv /usr/lib/aarch64-linux-gnu/libSDL2.so /usr/lib/aarch64-linux-gnu/libSDL2-2.0.so.0 | tee -a "$LOG_FILE"
+      sudo ln -sfv /usr/lib/aarch64-linux-gnu/libSDL2-2.0.so.0 /usr/lib/aarch64-linux-gnu/libSDL2-2.0.so | tee -a "$LOG_FILE"
+      sudo ln -sfv /usr/lib/aarch64-linux-gnu/libSDL2-2.0.so.0.3000.7 /usr/lib/aarch64-linux-gnu/libSDL2.so | tee -a "$LOG_FILE"
+      sudo ln -sfv /usr/lib/arm-linux-gnueabihf/libSDL2.so /usr/lib/arm-linux-gnueabihf/libSDL2-2.0.so.0 | tee -a "$LOG_FILE"
+      sudo ln -sfv /usr/lib/arm-linux-gnueabihf/libSDL2-2.0.so.0 /usr/lib/arm-linux-gnueabihf/libSDL2-2.0.so | tee -a "$LOG_FILE"
+      sudo ln -sfv /usr/lib/arm-linux-gnueabihf/libSDL2-2.0.so.0.3000.7 /usr/lib/arm-linux-gnueabihf/libSDL2.so | tee -a "$LOG_FILE"
+
+	touch "/home/ark/.config/.update11062024"
 
 fi
 
